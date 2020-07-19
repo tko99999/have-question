b0daa007f611b1f871021efdfd5dd19237062b82
tko99999 <araitakao@arii-inc.com>
2020-07-20 00:48:56 +0900

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'questions#new'
+
+  get 'admin/register', to: 'admins#new'
+  post 'admin/register', to: 'admins#create'
+  get 'admin/login', to: 'sessions#new'
+  post 'admin/login', to: 'sessions#create'
+  delete 'admin/logout', to: 'sessions#destroy'
+  resources :questions, param: :token
 end
